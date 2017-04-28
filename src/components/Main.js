4c8b61562ae5ded73343235caabe77642123097e
chujunfang <chujunfang@qfpay.com>
2017-04-28 10:24:37 +0800

    搭建背景

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,49 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/main.css');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+//改行不用
+//let yeomanImage = require('../images/img15.png');
+//获取图片数据
+let imageDatas = require('../data/imageDatas.json');
 
+// function genImageUrl(img) {
+//     for (var i = 0, j = img.length; i < j; i++) {
+//         var singleImg = img[i];
+//         singleImg.imageUrl = require('../images/' + singleImg.filename);
+//         img[i] = singleImg;
+//     }
+//     return img;
+// }
+// imageDatas = genImageUrl(imageDatas);
+
+//将图片名信息转成url路径信息
+imageDatas = (function genImageUrl(img) {
+    for (var i = 0, j = img.length; i < j; i++) {
+        var singleImg = img[i];
+        singleImg.imageUrl = require('../images/' + singleImg.filename);
+        img[i] = singleImg;
+    }
+    return img;
+})(imageDatas);
 class AppComponent extends React.Component {
-  render() {
-    return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <span>hello</span>
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
-    );
-  }
+    render() {
+        // return ( < div className = "index" >
+        //     < img src = { yeomanImage }
+        //     alt = "Yeoman Generator" / >
+        //     < span > hello < /span> < div className = "notice" > Please edit < code > src /
+        //     components / Main.js < /code> to get started!</div >
+        //     < /div>
+        // );
+        return ( < section className = "stage" >
+            < section className = "img-sec" >
+            < /section> < nav className = "controller-nav" >
+            < /nav> < /section>
+        );
+    }
 }
 
-AppComponent.defaultProps = {
-};
+AppComponent.defaultProps = {};
 
 export default AppComponent;
